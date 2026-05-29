cf2e39bf803a484586acc88122d4b97878018305
sanketsbrush <95219374+sanketsbrush@users.noreply.github.com>
2026-05-29 12:38:28 +0530

    Delete Extras directory

--- Extras/Demo/execute_string.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-@onready var text_edit_auto_saver: TextEditAutoSaver = $"../TextEditAutoSaver"
-
-func _ready() -> void:
-	pressed.connect(on_pressed)
-
-func on_pressed():
-	var result = EF.Execute_String(text_edit_auto_saver.text)
-	text_edit_auto_saver.text+= "\n\nAns: "+str(result)
--- Extras/Demo/icon.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite2D
-
-func _process(delta: float) -> void:
-	position.y+=Input.get_axis("ui_up","ui_down")
